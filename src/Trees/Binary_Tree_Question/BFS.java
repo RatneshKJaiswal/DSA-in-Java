c5e88873ae82a64d9e5d0af20605a4571a47a25c
RatneshKJaiswal <ratnesh.kr.jais@gmail.com>
2024-06-30 14:32:29 +0530

    BFS

---
+++ src/Trees/Binary_Tree_Question/BFS.java
@@ -0,0 +1,57 @@
+package Trees.Binary_Tree_Question;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFS {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+        }
+    }
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(root==null){
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+
+            int levelSize = queue.size();
+            List<Integer> curLevel = new ArrayList<>();
+
+            for(int i = 0 ;i<levelSize ; i++){
+
+                TreeNode curNode = queue.poll();
+                curLevel.add(curNode.val);
+
+                if(curNode.left!=null){
+                    queue.offer(curNode.left);
+                }
+                if(curNode.right!=null){
+                    queue.offer(curNode.right);
+                }
+            }
+            res.add(curLevel);
+        }
+
+
+        return res;
+    }
+}
